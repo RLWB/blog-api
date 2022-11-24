d1cdb609f82a54da83b525be8265e2f55c8ab934
huirui <rlwb1017669154@gmail.com>
2022-11-25 00:07:32 +0800

    dev

--- router/collection.js
+++ router/collection.js
@@ -1,26 +1,27 @@
 const router = require("express").Router();
 const User = require("../model/User");
 const Post = require("../model/Post");
+const Collection = require("../model/Collection");
 const handleToken = require("../middleWares");
 //收藏，取消收藏
 router.post("/", handleToken, async (req, res) => {
   const id = req.body.id;
   try {
-    const post = await Post.findById(id);
     const user = await User.findById(req.user._id);
-    const index = post.collectionsIds.findIndex((item) => item == req.user._id);
+    const index = user.collections.indexOf(id);
     if (index >= 0) {
-      post.collectionsIds.splice(index, 1);
-      user.collections.splice(user.collections.indexOf(id), 1);
+      user.collections.splice(index, 1);
     } else {
-      post.collectionsIds.unshift(req.user._id);
       user.collections.unshift(id);
     }
-    await post.save();
-    await user.save();
+    await User.updateOne(
+      {
+        _id: req.user._id,
+      },
+      user
+    );
     res.status(200).json("操作成功");
   } catch (error) {
-    console.log(error);
     res.status(500).json(error);
   }
 });
